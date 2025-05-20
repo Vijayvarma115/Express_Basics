3a585112955a184a0672b1bc30dbefa64aa06a9a
Vijay Varma <145138789+Vijayvarma115@users.noreply.github.com>
2025-05-21 01:39:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,3 +109,66 @@ app.delete("/",(req,res)=>{
 
 
 app.listen(3000);
+
+
+
+
+
+
+////////////////////////////////////ASSIGNMENT/////////////////////////////
+
+/**
+  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+  - Use built in Node.js `fs` module
+  The expected API endpoints are defined below,
+  1. GET /files - Returns a list of files present in `./files/` directory
+    Response: 200 OK with an array of file names in JSON format.
+    Example: GET http://localhost:3000/files
+  2. GET /file/:filename - Returns content of given file by name
+     Description: Use the filename from the request path parameter to read the file from `./files/` directory
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Example: GET http://localhost:3000/file/example.txt
+    - For any other route not defined in the server return 404
+    Testing the server - run `npm run test-fileServer` command in terminal
+ */
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+const fpath='./folder';
+
+app.get("/files",(req,res)=>{
+    fs.readdir(fpath,(err,files)=>{
+
+    const filenames=[];
+
+    for(const file of files){
+        const full=path.join(fpath,file);
+        const stat=fs.statSync(full);
+
+        if(stat.isFile()){
+            filenames.push(file);
+        }
+    }
+
+    res.json(filenames)
+})
+})
+
+
+app.get("/files/:filename",(req,res)=>{
+    const name=req.params.filename;
+    const full1=path.join(fpath,name);
+
+    fs.readFile(full1,"utf-8",(err,data)=>{
+        if(err){
+            res.send("Error");
+        }
+        res.send({data});
+    })
+})
+
+
+app.listen(3000);
+module.exports = app;
